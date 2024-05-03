cc701c14b6afa15cc7509e89b17618e160943c7b
Matheus Chagas <matheuschagas29@gmail.com>
2024-05-02 21:58:07 -0300

    loop

--- javascript/0208-arrays-e-loops/script.js
+++ javascript/0208-arrays-e-loops/script.js
@@ -1,23 +1,27 @@
 // Crie uma array com os anos que o Brasil ganhou a copa
 // 1959, 1962, 1970, 1994, 2002
-var brasilCampeao = [1959, 1962, 1970, 1994, 2002];
 
+var ganhou = [1959, 1962, 1970, 1994, 2002];
 // Interaja com a array utilizando um loop, para mostrar
+for (let i=0;i<ganhou.length;i++){
+  console.log(ganhou[i]);
+}
 // no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
-for (var i = 0; i < brasilCampeao.length; i++) {
-  console.log(`O brasil ganhou a copa de ${brasilCampeao[i]}`);
+for (let i=0;i<ganhou.length;i++){
+  console.log(`O brasil ganhou a copa de ${ganhou[i]}`);
 }
 
 // Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
-var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
-for (var fruta = 0; fruta < frutas.length; fruta++) {
-  console.log(frutas[fruta]);
-  if(frutas[fruta] === 'Pera') {
+var fruta = ['manga','acerola','uva','pera','laranja']
+for ( let i=0;i<fruta.length;i++){
+  console.log(fruta[i]);
+  if (fruta[i]==='pera'){
     break;
   }
 }
 
 // Coloque a última fruta da array acima em uma variável,
 // sem remover a mesma da array.
+var ultimaFruta = fruta[fruta.length-1];
+console.log(ultimaFruta)
 
-var ultimaFruta = frutas[frutas.length - 1];
