9b181b40941327e7cc5c9cb8c27a46dc53c41173
Matheus Chagas <matheuschagas29@gmail.com>
2024-06-03 23:34:33 -0300

    first projeto rockeseat

--- navegacao-acordeion(quase full)/script.js
+++ navegacao-acordeion(quase full)/script.js
@@ -29,6 +29,7 @@ tabMenu.forEach((itemMenu,index)=>{
 }
 initTabNva();
 
+//accordion
  function initAccordion(){
 const accordionList = document.querySelectorAll('.js-accordion dt')
 if(accordionList.length){
@@ -47,3 +48,49 @@ accordionList.forEach((item)=> {
 }
 
 initAccordion();
+
+// sroll devagavar para o link interno
+function initScrollSuave () {
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event){
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+
+  section.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', scrollToSection);
+});
+}
+
+initScrollSuave();
+
+// animação para o scroll
+function InitanimacaoScroll(){
+const sections = document.querySelectorAll('.js-scroll');
+
+if (sections.length){
+const windown60 = window.innerHeight * 0.6
+function animaScroll() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top ;   
+    const isSectionVisible = (sectionTop-windown60)< 0;
+    if (isSectionVisible ) {
+      section.classList.add('ativo');
+    }
+  })
+}
+
+animaScroll();
+
+window.addEventListener('scroll', animaScroll);
+}
+}
+
+InitanimacaoScroll();
