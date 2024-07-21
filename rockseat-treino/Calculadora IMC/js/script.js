e527802f648e73d6d50223b242ce234c2663b5ca
Matheus Chagas <matheuschagas29@gmail.com>
2024-07-20 22:44:26 -0300

    att fundamentos SPA

--- rockseat-treino/Calculadora IMC/js/script.js
+++ rockseat-treino/Calculadora IMC/js/script.js
@@ -1,4 +1,5 @@
 import {Modal} from './modal.js'
+import {AlertError}from './alert-error.js'
 
 const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
@@ -12,13 +13,26 @@ form.onsubmit = function(event){
   event.preventDefault()
   const weight = Number(inputWeight.value); 
   const height = Number(inputHeight.value);
+  console.log(notNumber(weight));
+  console.log(notNumber(height));
+
+  const showAlertError=notNumber(weight)|| notNumber(height)
+
+  if(showAlertError){
+    AlertError.open();
+    return;
+  }
+  AlertError.close()
+
   const result = IMC (weight,height);
   const Message = `Seu IMC é de ${result}`
     Modal.open()
   Modal.Message.innerText = Message
 }
 
-
+function notNumber(value){
+  return isNaN(value) || value == "";
+}
 
 function IMC (weight,height){
   return (weight/((height/100)**2)).toFixed(2);
